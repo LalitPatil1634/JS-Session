83bee3c6fdf996b1ffe437c98e5e0f70b9a715b1
Lalit LP <64311299+LalitPatil1634@users.noreply.github.com>
2022-07-13 09:22:13 +0530

    Dom and bom

---
+++ src/DOM/event-listner.js
@@ -0,0 +1,5 @@
+const helloDiv = document.querySelector("#app1");
+
+const handleClick = () => console.log("Hello Div");
+
+helloDiv.addEventListener("click", handleClick);
--- src/Object-Modeling/BOM.js
+++ src/Object-Modeling/BOM.js
@@ -1,4 +1,37 @@
 // const div = document.getElementById("app");
 // div.innerHTML = "Bye";
+/*
+The Browser Object Model (BOM) in
+JavaScript includes the properties and
+methods for JavaScript to interact with
+the web browser. BOM provides you with
+a window objects, for example, 
+to show the width and height of the window. 
+It also includes the window. screen object to
+show the width and height of the screen.
+*/
+// console.log(document.getElementById('header'));
 
-console.log(window.open);
+/*Two properties can be used to determine
+the size of the browser window.
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+*/
+// console.log(window.open());
+// console.log(window.close);
+// console.log(window.moveTo);
+// console.log(window.resizeTo);
+
+// Window Screen
+// document.getElementById('app').innerHTML ="Screen Height = " + screen.height;
+// document.getElementById('app').innerHTML ="Screen Width = " + screen.width;
+// document.getElementById('app').innerHTML = "Available Screen Width " + screen.availWidth;
+// document.getElementById('app').innerHTML = "Available Screen Height " + screen.availHeight;
+// document.getElementById('app').innerHTML = "Screen color depth " + screen.colorDepth;
+// document.getElementById('app').innerHTML = "Screen pixel depth " + screen.pixelDepth;
+
+// Window Location
+// document.getElementById('app').innerHTML = "Page location is " + window.location.href;
+// document.getElementById('app').innerHTML = "Page hostname is " + window.location.hostname;
+// document.getElementById('app').innerHTML = "Page path is " + window.location.pathname;
+// document.getElementById('app').innerHTML = "Port number is " + window.location.port;
